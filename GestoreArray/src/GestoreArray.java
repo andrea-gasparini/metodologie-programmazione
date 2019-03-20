529c9e543e1eeb23499373384a63c3c8f9acf521
Andrea Gasparini <36965464+gasparini-1813486@users.noreply.github.com>
2019-03-20 14:39:06 +0100

    Add files via upload

--- GestoreArray/src/GestoreArray.java
+++ GestoreArray/src/GestoreArray.java
@@ -30,7 +30,7 @@ public class GestoreArray
 	/**
 	 * Concatena un altro array di interi in fondo all'array dell'oggetto
 	 *
-	 * @param array un array di interi da concatenare
+	 * @param un array di interi da concatenare
 	 */
 	public void concat(int[] array) 
 	{
@@ -49,35 +49,56 @@ public class GestoreArray
 	/**
 	 * Concatena un array di un altro oggetto in fondo all'array dell'oggetto principale
 	 *
-	 * @param array un oggetto array di interi da concatenare
+	 * @param un oggetto array di interi da concatenare
 	 */
 	public void concat(GestoreArray array) { concat(array.array); }
 	
 	/**
-	 * Preso in input un altro array di interi, concatena in fondo all'array dell'oggetto
-     * solo gli elementi che non sono già presenti. Il metodo restituisce il numero di elementi scartati
+	 * Concatena un altro array di interi in fondo all'array dell'oggetto, escludendo le ripetizioni
+     * Il metodo restituisce il numero di elementi scartati
 	 *
 	 * @param array un array di interi da concatenare senza ripetizioni
-	 * @return duplicati numero di elementi scartati dalla concatenazione
+	 * @return numero di elementi scartati dalla concatenazione
 	 */
 	public int concatNoDup(int[] array)
 	{
 		int duplicati = 0;
-		int[] array2 = new int[this.array.length + array.length];
-		int i;
 		
-		for(i = 0; i < this.array.length; i++)
-			array2[i] = this.array[i];
-		
-		for(int ii = 0; ii < array.length; ii++, i++)
-		{
-			//this.array.indexOf(array[i]);
-			array2[i] = array[ii];
-		}
-		
-		this.array = array2;
+		for(int i = 0; i < array.length; i++)
+			if(indexOf(array[i]) != -1) 
+				duplicati++;
+			else
+				concat(new int[] { array[i] });
 		
 		return duplicati;
+	}
+	
+	/**
+	 * Concatena un array di un altro oggetto in fondo all'array dell'oggetto principale, escludendo le ripetizioni
+	 *
+	 * @param array un oggetto array di interi da concatenare
+	 */
+	public int concatNoDup(GestoreArray array) { return concatNoDup(array.array); } 
+	
+	/**
+	 * Sostituisce la prima occorrenza di x con il valore di y all’interno dell’array
+	 * Se l’occorrenza viene sostituita, il metodo restituisce true, altrimenti false
+	 * 
+	 * @param x valore del vettore da sostituire
+	 * @param y valore con cui sostituire la prima occorrenza di x
+	 * @return true se ha effettuato la sostituzione, false altrimenti
+	 */
+	public boolean replace(int x, int y) 
+	{
+		boolean sostituto = false;
+		
+		if(indexOf(x) != -1)
+		{
+			array[indexOf(x)] = y;
+			sostituto = true;
+		}
+		
+		return sostituto;
 	}
 	
 	/**
@@ -100,12 +121,10 @@ public class GestoreArray
 	
 	public static void main(String[] args)
 	{
-		int[] myArray = {2, 4, 6, 10};
-		GestoreArray a = new GestoreArray(new int[] {1, 2, 3});
+		int[] myArray = {4, 4, 6, 10};
+		GestoreArray a = new GestoreArray(new int[] {2, 2, 3});
 		GestoreArray b = new GestoreArray(myArray);
 		
-		System.out.println(a.indexOf(3)); 
-		System.out.println(a.indexOf(4));
 		a.concat(b);
 		a.concat(new int[] {4, 5, 6, 7});
 		
