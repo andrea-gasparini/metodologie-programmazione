95f7851ca12cc5ba12feebf7498e5da8a3699e8a
Andrea Gasparini <gasptube@gmail.com>
2020-01-30 21:36:56 +0100

    Delete TypeScript progetto

--- metodologie2019_progetto/FabbricaSemanticaJSweet/target/ts/quickstart/PageType.ts
+++
@@ -1,7 +0,0 @@
-/* Generated from Java with JSweet 2.3.0-SNAPSHOT - http://www.jsweet.org */
-namespace quickstart {
-    export enum PageType {
-        LOGIN_PAGE, SIGNUP_PAGE, HOME_PAGE, ANNOTATION_GAME_PAGE
-    }
-}
-
